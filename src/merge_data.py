c8fa6d3c0b1b092b96d66f2bf1e5307f4852a59f
FalconCod <pranay.shiva2004@gmail.com>
2026-07-08 22:12:40 +0530

    add: merged JGB + USDJPY dataset with inner join

---
+++ src/merge_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from data_loader import load_jgb_data, filter_window
+from fx_loader import load_usdjpy
+
+def build_merged_dataset(start_year=2011):
+    jgb = load_jgb_data()
+    jgb = filter_window(jgb, start_year=start_year)
+    jgb = jgb.set_index('Date')
+
+    fx = load_usdjpy(start=f"{start_year}-01-01")
+
+    merged = jgb[['1Y', '10Y']].join(fx, how='inner')
+    return merged
+
+if __name__ == "__main__":
+    merged = build_merged_dataset()
+    print(merged.head())
+    print(merged.tail())
+    print(merged.shape)
+    print(merged.isna().sum())
